5f99fe2495cb600ef2263e46782a1f2c64ca190c
Guido Lorenz <guido.lorenz@artcom.de>
2015-11-10 14:53:41 +0100

    Use actions when printing status

--- src/commands/status.js
+++ src/commands/status.js
@@ -4,75 +4,51 @@ import _ from "lodash"
 import colors from "colors/safe"
 import table from "text-table"
 
-import envCommand from "./envCommand"
+import {deriveActions} from "../actions"
+import ConfigAction from "../actions/configAction"
+import DockerOptionAction from "../actions/dockerOptionAction"
+import PushAction from "../actions/pushAction"
 import {loadAppData} from "../appData"
+import envCommand from "./envCommand"
 
-import type {AppData} from "../appData"
+import type {Action} from "../actions"
+import type {AppData, DeployedAppData} from "../appData"
 import type {Environment, ServiceConfigs} from "../types"
 
 export default envCommand(function(environment: Environment, configs: ServiceConfigs) {
   return loadAppData(environment, configs).then(function(appData) {
     const rows = appData.map((app) => {
-      return [
-        printName(app),
-        printStatus(app),
-        printVersion(app),
-        print("config", app),
-        print("dockerOptions", app)
-      ]
+      switch (app.status) {
+        case "missing":
+          return [name(app), colors.red("missing")]
+        case "deployed":
+          const actions = deriveActions(app)
+          const color = _.isEmpty(actions) ? colors.green : colors.yellow
+          return [name(app), color("deployed")].concat(deploymentStatus(app, actions))
+        case "additional":
+          return [name(app), colors.gray("additional")]
+        default:
+          return []
+      }
     })
 
     console.log(table(rows))
   })
 })
 
-function printName(app: AppData) {
+function name(app: AppData) {
   return colors.bold(app.name)
 }
 
-function printStatus(app: AppData) {
-  const color = statusColor(app)
-  return color(app.status)
+function deploymentStatus(app: DeployedAppData, actions: Array<Action>) {
+  return [
+    colorize(app.deployed.version, _.some(actions, (action) => action instanceof PushAction)),
+    colorize("config", _.some(actions, (action) => action instanceof ConfigAction)),
+    colorize("dockerOptions", _.some(actions, (action) => action instanceof DockerOptionAction))
+  ]
 }
 
-function statusColor(app: AppData) {
-  switch (app.status) {
-    case "deployed":
-      if (_.every(["version", "config", "dockerOptions"].map(isUpToDate(app)))) {
-        return colors.green
-      }
-
-      return colors.yellow
-
-    case "missing":
-      return colors.red
-
-    case "additional":
-      return colors.gray
-
-    default:
-      return colors.white
-  }
+function colorize(string: string, dirty: boolean) {
+  const color = dirty ? colors.red : colors.green
+  return color(string)
 }
-
-function printVersion(app: AppData) {
-  if (app.status === "deployed") {
-    const color = isUpToDate(app, "version") ? colors.green : colors.red
-    return color(app.deployed.version)
-  } else {
-    return ""
-  }
-}
-
-function print(key, app: AppData) {
-  if (app.status === "deployed") {
-    const color = isUpToDate(app, key) ? colors.green : colors.red
-    return color(key)
-  } else {
-    return ""
-  }
-}
-
-const isUpToDate = _.curry(function(app, key) {
-  return _.isEqual(app.config[key], app.deployed[key])
-})
