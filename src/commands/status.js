bb899643c02ff71d72d5f2b64738fb86d6cf6fd2
Guido Lorenz <guido.lorenz@artcom.de>
2015-11-10 09:34:24 +0100

    Add config and dockerOptions to status output

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+import _ from "lodash"
 import colors from "colors/safe"
 import table from "text-table"
 
@@ -9,36 +10,75 @@ import * as status from "../status"
 import type {Status} from "../status"
 import type {Environment, Services} from "../types"
 
-const statusColor = {
-  deployed: colors.green,
-  missing: colors.red,
-  additional: colors.gray
-}
-
 export default envCommand(function(environment: Environment, services: Services) {
   return status.determine(environment, services).then(function(apps) {
     const rows = apps.map((app) => {
-      return [formatName(app), formatStatus(app), formatVersion(app)]
+      return [
+        printName(app),
+        printStatus(app),
+        printVersion(app),
+        print("config", app),
+        print("dockerOptions", app)
+      ]
     })
 
     console.log(table(rows))
   })
 })
 
-function formatName(app: Status) {
+function printName(app: Status) {
   return colors.bold(app.name)
 }
 
-function formatStatus(app: Status) {
-  const color = statusColor[app.type]
+function printStatus(app: Status) {
+  const color = statusColor(app)
   return color(app.type)
 }
 
-function formatVersion(app: Status) {
+function statusColor(app) {
+  switch (app.type) {
+    case "deployed":
+      if (_.every(["version", "config", "dockerOptions"].map(isUpToDate(app)))) {
+        return colors.green
+      }
+
+      return colors.yellow
+
+    case "missing":
+      return colors.red
+
+    case "additional":
+      return colors.gray
+
+    default:
+      return colors.white
+  }
+}
+
+function printVersion(app: Status) {
   if (app.type === "deployed") {
-    const color = app.version.expected === app.version.deployed ? colors.green : colors.red
-    return color(app.version.deployed)
+    const version = app.version
+    const color = isUpToDate(app, "version") ? colors.green : colors.red
+    return color(version.deployed)
   } else {
     return ""
   }
 }
+
+function print(key, app: Status) {
+  if (app.type === "deployed") {
+    const color = isUpToDate(app, key) ? colors.green : colors.red
+    return color(key)
+  } else {
+    return ""
+  }
+}
+
+const isUpToDate = _.curry(function(app, key) {
+  if (app.type === "deployed") {
+    const property = app[key]
+    return _.isEqual(property.expected, property.deployed)
+  } else {
+    return false
+  }
+})
