f4f1a98b62138bb12a0adb11825e06cfcb70be53
Guido Lorenz <guido.lorenz@artcom.de>
2016-01-25 16:24:29 +0100

    Display status data while loading

--- src/commands/status.js
+++ src/commands/status.js
@@ -3,6 +3,7 @@
 import _ from "lodash"
 import bluebird from "bluebird"
 import colors from "colors/safe"
+import logUpdate from "log-update"
 import table from "text-table"
 
 import {deriveActions} from "../actions"
@@ -19,34 +20,45 @@ import type {Environment, AppConfig} from "../types"
 export default envCommand(function(environment: Environment, configs: Array<AppConfig>) {
   return createProviderWithProgress(environment, configs).then((provider) => {
     const apps = provider.apps()
-    return bluebird.map(apps, (app) => provider.loadAppDataWithProgress(app).then(createRow), {
+    const data = {}
+    printTable(apps, data)
+
+    function updateAppData(appData) {
+      data[appData.name] = appData
+      printTable(apps, data)
+    }
+
+    return bluebird.map(apps, (app) => provider.loadAppData(app).then(updateAppData), {
       concurrency: 4
     })
-  }).then((rows) => {
-    console.log(table(rows))
   })
 })
+
+function printTable(apps, data) {
+  const rows = apps.map((app) => data[app] ? createRow(data[app]) : [name(app), "..."])
+  logUpdate(table(rows))
+}
 
 function createRow(app: AppData) {
   switch (app.status) {
     case "missing":
-      return [name(app), colors.red("missing")]
+      return [name(app.name), colors.red("missing")]
     case "created":
-      return [name(app), colors.red("created")]
+      return [name(app.name), colors.red("created")]
     case "deployed":
       const actions = deriveActions(app)
       const color = _.isEmpty(actions) ? colors.green : colors.yellow
       const running = app.running ? color("running") : colors.red("stopped")
-      return [name(app), running, version(app, actions), deploymentStatus(app, actions)]
+      return [name(app.name), running, version(app, actions), deploymentStatus(app, actions)]
     case "additional":
-      return [name(app), colors.gray(app.running ? "running" : "stopped")]
+      return [name(app.name), colors.gray(app.running ? "running" : "stopped")]
     default:
       return []
   }
 }
 
-function name(app: AppData) {
-  return colors.bold(app.name)
+function name(app: string) {
+  return colors.bold(app)
 }
 
 function version(app: DeployedAppData, actions: Array<Action>) {
