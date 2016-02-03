619e7fcb2347a9ebdf963db44d63046dd42480bc
Guido Lorenz <guido.lorenz@artcom.de>
2016-02-03 10:26:50 +0100

    Test deriveAction()

---
+++ test/deriveActions.js
@@ -0,0 +1,247 @@
+/* eslint-env mocha */
+
+import chai, { expect } from "chai"
+
+import { deriveActions } from "../src/actions"
+import ConfigAction from "../src/actions/configAction"
+import CreateAction from "../src/actions/createAction"
+import DockerOptionAction from "../src/actions/dockerOptionAction"
+import PushAction from "../src/actions/pushAction"
+import StartAction from "../src/actions/startAction"
+
+chai.use(function({ Assertion }) {
+  Assertion.addMethod("matchActionTypes", function(types) {
+    const actions = this._obj
+    new Assertion(actions).have.length(types.length, "wrong number of actions")
+    actions.forEach((action, i) => new Assertion(action).is.instanceof(types[i]))
+  })
+})
+
+const OLD_VERSION = "ef258538b0aa09870cd39fd4f6ae39dcb82cd3ac"
+const CURRENT_VERSION = "3c0d3cd3e131e4c56df95157d60868f38a9ce429"
+
+describe("deriveActions", function() {
+  describe("missing app", function() {
+    it("should create and deploy a missing app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "missing",
+        description: appDescription("app1", CURRENT_VERSION)
+      })
+
+      expect(actions).to.matchActionTypes([
+        CreateAction,
+        PushAction
+      ])
+    })
+
+    it("should create, configure and deploy a missing app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "missing",
+        description: appDescription("app1", CURRENT_VERSION, {
+          FOO: "bar"
+        }, {
+          "--some=option": ["build", "deploy"]
+        })
+      })
+
+      expect(actions).to.matchActionTypes([
+        CreateAction,
+        ConfigAction,
+        DockerOptionAction,
+        PushAction
+      ])
+    })
+  })
+
+  describe("created app", function() {
+    it("should deploy a created app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "created",
+        description: appDescription("app1", CURRENT_VERSION)
+      })
+
+      expect(actions).to.matchActionTypes([
+        PushAction
+      ])
+    })
+
+    it("should configure and deploy a created app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "created",
+        description: appDescription("app1", CURRENT_VERSION, {
+          FOO: "bar"
+        }, {
+          "--some=option": ["build", "deploy"]
+        })
+      })
+
+      expect(actions).to.matchActionTypes([
+        ConfigAction,
+        DockerOptionAction,
+        PushAction
+      ])
+    })
+  })
+
+  describe("deployed app", function() {
+    context("that is up-to-date", function() {
+      it("should ignore a running app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: true,
+          description: appDescription("app1", CURRENT_VERSION),
+          deployed: deployedConfig(CURRENT_VERSION)
+        })
+
+        expect(actions).to.be.empty
+      })
+
+      it("should start a stopped app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: false,
+          description: appDescription("app1", CURRENT_VERSION),
+          deployed: deployedConfig(CURRENT_VERSION)
+        })
+
+        expect(actions).to.matchActionTypes([
+          StartAction
+        ])
+      })
+    })
+
+    context("that is out-of-date", function() {
+      it("should deploy a running app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: true,
+          description: appDescription("app1", CURRENT_VERSION),
+          deployed: deployedConfig(OLD_VERSION)
+        })
+
+        expect(actions).to.matchActionTypes([
+          PushAction
+        ])
+      })
+
+      it("should deploy a stopped app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: false,
+          description: appDescription("app1", CURRENT_VERSION),
+          deployed: deployedConfig(OLD_VERSION)
+        })
+
+        expect(actions).to.matchActionTypes([
+          PushAction
+        ])
+      })
+
+      it("should configure and deploy a running app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: true,
+          description: appDescription("app1", CURRENT_VERSION, {
+            FOO: "bar"
+          }, {
+            "--some=option": ["build", "deploy"]
+          }),
+          deployed: deployedConfig(OLD_VERSION, {
+            OLD_COFIG: "old value"
+          }, {
+            "--some=option": ["deploy"]
+          })
+        })
+
+        expect(actions).to.matchActionTypes([
+          ConfigAction,
+          DockerOptionAction,
+          PushAction
+        ])
+      })
+
+      it("should configure and deploy a stopped app", function() {
+        const actions = deriveActions({
+          name: "app1",
+          status: "deployed",
+          running: false,
+          description: appDescription("app1", CURRENT_VERSION, {
+            FOO: "bar"
+          }, {
+            "--some=option": ["build", "deploy"]
+          }),
+          deployed: deployedConfig(OLD_VERSION, {
+            OLD_COFIG: "old value"
+          }, {
+            "--some=option": ["deploy"]
+          })
+        })
+
+        expect(actions).to.matchActionTypes([
+          ConfigAction,
+          DockerOptionAction,
+          PushAction
+        ])
+      })
+    })
+  })
+
+  describe("unknown app", function() {
+    it("should ignore running unknown app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "unknown",
+        running: true
+      })
+
+      expect(actions).to.be.empty
+    })
+
+    it("should ignore stopped unknown app", function() {
+      const actions = deriveActions({
+        name: "app1",
+        status: "unknown",
+        running: false
+      })
+
+      expect(actions).to.be.empty
+    })
+  })
+})
+
+function appDescription(
+  appName,
+  version,
+  config = {},
+  dockerOptions = {}
+) {
+  return {
+    name: appName,
+    repo: `git@github.com:artcom/${appName}.git`,
+    version,
+    config,
+    dockerOptions,
+    stopBeforeDeployment: false
+  }
+}
+
+function deployedConfig(
+  version,
+  config = {},
+  dockerOptions = {}
+) {
+  return {
+    version,
+    config,
+    dockerOptions
+  }
+}
