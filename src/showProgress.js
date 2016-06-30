artcom/dock-worker

commit 12823317db99f16c6d0344a2ff5cdcde3450bfb9

Use async functions

src/showProgress.js
@@ -1,26 +1,17 @@
 import elegantSpinner from "elegant-spinner"
 import logUpdate from "log-update"
 
-export default function(message, promise) {
+export default async function(message, promise) {
   const spinner = elegantSpinner()
 
   const interval = setInterval(() => {
     logUpdate(message(spinner()))
   }, 66)
 
-  function stop() {
+  try {
+    return await promise
+  } finally {
     clearInterval(interval)
     logUpdate.clear()
   }
-
-  return promise.then(
-    (result) => {
-      stop()
-      return result
-    },
-    (error) => {
-      stop()
-      throw error
-    }
-  )
 }

.babelrc
@@ -1,6 +1,8 @@
 {
   "plugins": [
+    "syntax-async-functions",
     "syntax-flow",
+    "transform-async-to-generator",
     "transform-es2015-modules-commonjs",
     "transform-flow-strip-types"
   ]

src/actions/configAction.js
@@ -22,11 +22,11 @@ export default class {
     return this.changes.map(describeChange)
   }
 
-  run(dokku: Dokku): Promise {
+  async run(dokku: Dokku): Promise {
     const [unset, set] = _.partition(this.changes, (change) => change.type === "remove")
 
-    return setConfig(dokku, this.description, set)
-      .then(() => unsetConfig(dokku, this.description, unset))
+    await setConfig(dokku, this.description, set)
+    await unsetConfig(dokku, this.description, unset)
   }
 }
 
@@ -43,13 +43,15 @@ function describeChange(change: Change): string {
   }
 }
 
-function stopApp(dokku, description) {
-  return description.stopBeforeDeployment ? dokku.stop(description.name) : Promise.resolve()
+async function stopApp(dokku, description) {
+  if (description.stopBeforeDeployment) {
+    await dokku.stop(description.name)
+  }
 }
 
-function setConfig(dokku, description, changes) {
+async function setConfig(dokku, description, changes) {
   if (_.isEmpty(changes)) {
-    return Promise.resolve()
+    return
   }
 
   const options = _.chain(changes)
@@ -57,14 +59,17 @@ function setConfig(dokku, description, changes) {
     .fromPairs()
     .value()
 
-  return stopApp(dokku, description).then(() => dokku.setConfig(description.name, options))
+  await stopApp(dokku, description)
+  await dokku.setConfig(description.name, options)
 }
 
-function unsetConfig(dokku, description, changes) {
+async function unsetConfig(dokku, description, changes) {
   if (_.isEmpty(changes)) {
-    return Promise.resolve()
+    return
   }
 
   const vars = _.map(changes, "key")
-  return stopApp(dokku, description).then(() => dokku.unsetConfig(description.name, ...vars))
+
+  await stopApp(dokku, description)
+  await dokku.unsetConfig(description.name, ...vars)
 }

src/actions/dockerOptionAction.js
@@ -1,7 +1,5 @@
 /* @flow */
 
-import bluebird from "bluebird"
-
 import diffOptions from "../diffOptions"
 import Dokku from "../dokku"
 
@@ -22,17 +20,19 @@ export default class {
     return this.changes.map(describeChange)
   }
 
-  run(dokku: Dokku): Promise {
+  async run(dokku: Dokku): Promise {
     const appName = this.description.name
 
-    return bluebird.each(this.changes, (change) => applyChange(dokku, appName, change))
-      .then(() => {
-        if (this.description.stopBeforeDeployment) {
-          return dokku.stop(appName).then(() => dokku.start(appName))
-        } else {
-          return dokku.restart(appName)
-        }
-      })
+    for (const change of this.changes) {
+      await applyChange(dokku, appName, change)
+    }
+
+    if (this.description.stopBeforeDeployment) {
+      await dokku.stop(appName)
+      await dokku.start(appName)
+    } else {
+      await dokku.restart(appName)
+    }
   }
 }
 
@@ -48,25 +48,25 @@ function describeChange(change: Change): string {
   }
 }
 
-function applyChange(dokku: Dokku, appName: string, change: Change): Promise {
+async function applyChange(dokku: Dokku, appName: string, change: Change): Promise {
   switch (change.type) {
     case "add": {
       const { key, value } = change
-      return dokku.addDockerOption(appName, key, value)
+      await dokku.addDockerOption(appName, key, value)
+      break
     }
 
     case "update": {
       const { key, value, oldValue } = change
-      return dokku.removeDockerOption(appName, key, oldValue)
-        .then(() => dokku.addDockerOption(appName, key, value))
+      await dokku.removeDockerOption(appName, key, oldValue)
+      await dokku.addDockerOption(appName, key, value)
+      break
     }
 
     case "remove": {
       const { key, oldValue } = change
-      return dokku.removeDockerOption(appName, key, oldValue)
+      await dokku.removeDockerOption(appName, key, oldValue)
+      break
     }
-
-    default:
-      return Promise.resolve()
   }
 }

src/actions/pushAction.js
@@ -16,17 +16,16 @@ export default class {
     return [`deploy ${this.description.version}`]
   }
 
-  run(dokku: Dokku, repoCache: RepoCache): Promise {
-    const stopApp = this.description.stopBeforeDeployment
-      ? dokku.stop(this.description.name)
-      : Promise.resolve()
+  async run(dokku: Dokku, repoCache: RepoCache): Promise {
+    if (this.description.stopBeforeDeployment) {
+      await dokku.stop(this.description.name)
+    }
 
     const { ORIGIN_REMOTE, DOKKU_REMOTE } = repoCache
 
-    return stopApp
-      .then(() => repoCache.getRepo(this.description.name))
-      .then((repo) => repo.ensureRemote(ORIGIN_REMOTE, this.description.repo))
-      .then((repo) => repo.fetch(ORIGIN_REMOTE))
-      .then((repo) => repo.push(DOKKU_REMOTE, `${this.description.version}:refs/heads/master`))
+    const repo = await repoCache.getRepo(this.description.name)
+    await repo.ensureRemote(ORIGIN_REMOTE, this.description.repo)
+    await repo.fetch(ORIGIN_REMOTE)
+    await repo.push(DOKKU_REMOTE, `${this.description.version}:refs/heads/master`)
   }
 }

src/appData.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 import _ from "lodash"
-import bluebird from "bluebird"
 
 import Dokku from "./dokku"
 import RepoCache from "./repoCache"
@@ -50,22 +49,21 @@ class Context {
     this.repoCache = repoCache
   }
 
-  initialize(): Promise<Context> {
-    return this.dokku.ls().then((list) => {
-      const available = _.map(list, "name")
-      const defined = _.map(this.descriptions, "name")
+  async initialize(): Promise<Context> {
+    const list = await this.dokku.ls()
+    const available = _.map(list, "name")
+    const defined = _.map(this.descriptions, "name")
 
-      this.status = _.chain(list)
-        .map(({ name, status }) => [name, status])
-        .fromPairs()
-        .value()
+    this.status = _.chain(list)
+      .map(({ name, status }) => [name, status])
+      .fromPairs()
+      .value()
 
-      this.missingApps = _.difference(defined, available)
-      this.existingApps = _.intersection(defined, available)
-      this.unknownApps = _.difference(available, defined)
+    this.missingApps = _.difference(defined, available)
+    this.existingApps = _.intersection(defined, available)
+    this.unknownApps = _.difference(available, defined)
 
-      return this
-    })
+    return this
   }
 
   listAppNames(): Array<string> {
@@ -90,8 +88,8 @@ class Context {
     }
   }
 
-  missingAppData(description: AppDescription): Promise<KnownAppData> {
-    return Promise.resolve({
+  async missingAppData(description: AppDescription): Promise<KnownAppData> {
+    return {
       name: description.name,
       status: "missing",
       deployed: false,
@@ -102,48 +100,48 @@ class Context {
         config: {},
         dockerOptions: {}
       }
-    })
+    }
   }
 
-  existingAppData(description: AppDescription, status: string): Promise<KnownAppData> {
+  async existingAppData(description: AppDescription, status: string): Promise<KnownAppData> {
     const deployed = status !== "NOT_DEPLOYED"
     const running = status === "running"
 
-    return bluebird.props({
-      version: deployed ? this.actualVersion(description.name) : "",
-      config: this.dokku.config(description.name),
-      dockerOptions: this.dokku.dockerOptions(description.name)
-    }).then((actual) => ({
+    return {
       name: description.name,
       status: "exists",
       deployed,
       running,
       description,
-      actual
-    }))
+      actual: {
+        version: deployed ? await this.actualVersion(description.name) : "",
+        config: await this.dokku.config(description.name),
+        dockerOptions: await this.dokku.dockerOptions(description.name)
+      }
+    }
   }
 
-  unknownAppData(name: string, status: string): Promise<UnknownAppData> {
+  async unknownAppData(name: string, status: string): Promise<UnknownAppData> {
     const deployed = status !== "NOT_DEPLOYED"
     const running = status === "running"
 
-    return bluebird.props({
-      version: deployed ? this.actualVersion(name) : "",
-      config: this.dokku.config(name),
-      dockerOptions: this.dokku.dockerOptions(name)
-    }).then((actual) => ({
+    return {
       name,
       status: "unknown",
       deployed,
       running,
-      actual
-    }))
+      actual: {
+        version: deployed ? await this.actualVersion(name) : "",
+        config: await this.dokku.config(name),
+        dockerOptions: await this.dokku.dockerOptions(name)
+      }
+    }
   }
 
-  actualVersion(name: string): Promise<string> {
-    return this.repoCache.getRepo(name)
-      .then((repo) => repo.fetch(this.repoCache.DOKKU_REMOTE))
-      .then((repo) => repo.showRef(`refs/remotes/${this.repoCache.DOKKU_REMOTE}/master`))
+  async actualVersion(name: string): Promise<string> {
+    const repo = await this.repoCache.getRepo(name)
+    await repo.fetch(this.repoCache.DOKKU_REMOTE)
+    return await repo.showRef(`refs/remotes/${this.repoCache.DOKKU_REMOTE}/master`)
   }
 }
 

src/commands/deploy.js
@@ -25,7 +25,7 @@ type AppActions = {
 
 export default envCommand(deploy)
 
-function deploy(
+async function deploy(
   descriptions: Array<AppDescription>,
   dokku: Dokku,
   repoCache: RepoCache,
@@ -34,14 +34,13 @@ function deploy(
   const selectedApps = options["<app>"]
   validateSelectedApps(descriptions, selectedApps)
 
-  return showProgress(
+  const context = await showProgress(
     (spinner) => chalk.gray(`loading service list ${spinner}`),
     loadContext(descriptions, dokku, repoCache)
-  ).then((context) =>
-    loadAppActions(context, descriptions, selectedApps)
-  ).then((appActions) =>
-    applyAppActions(appActions, dokku, repoCache, options["--yes"])
   )
+
+  const appActions = await loadAppActions(context, descriptions, selectedApps)
+  await applyAppActions(appActions, dokku, repoCache, options["--yes"])
 }
 
 function validateSelectedApps(descriptions, selectedApps) {
@@ -52,7 +51,7 @@ function validateSelectedApps(descriptions, selectedApps) {
   })
 }
 
-function loadAppActions(context, descriptions, selectedApps) {
+async function loadAppActions(context, descriptions, selectedApps) {
   const appNames = context.listAppNames().filter((appName) => {
     const description = _.find(descriptions, ["name", appName])
     return description && hasBeenSelected(description, selectedApps)
@@ -60,23 +59,24 @@ function loadAppActions(context, descriptions, selectedApps) {
 
   const appActionsList: Array<AppActions> = appNames.map((appName) => ({ appName }))
 
-  function deriveAppAction(appName) {
-    return context.loadAppData(appName)
-      .then(deriveActions)
-      .then((actions) => {
-        if (actions.length > 0) {
-          const appActions = _.find(appActionsList, { appName })
-          appActions.actions = actions
-        } else {
-          _.remove(appActionsList, { appName })
-        }
-      })
+  async function deriveAppAction(appName) {
+    const appData = await context.loadAppData(appName)
+    const actions = deriveActions(appData)
+
+    if (actions.length > 0) {
+      const appActions = _.find(appActionsList, { appName })
+      appActions.actions = actions
+    } else {
+      _.remove(appActionsList, { appName })
+    }
   }
 
-  return showProgress(
+  await showProgress(
     (spinner) => printList(appActionsList, spinner),
     bluebird.map(appNames, deriveAppAction, { concurrency: 4 })
-  ).then(() => appActionsList)
+  )
+
+  return appActionsList
 }
 
 function hasBeenSelected(description: AppDescription, selectedApps: Array<string>) {
@@ -87,35 +87,35 @@ function hasBeenSelected(description: AppDescription, selectedApps: Array<string
   }
 }
 
-function applyAppActions(appActionsList, dokku, repoCache, yes) {
+async function applyAppActions(appActionsList, dokku, repoCache, yes) {
   if (appActionsList.length === 0) {
     return console.log("nothing to deploy")
   }
 
   console.log(printList(appActionsList))
-  const confirm = yes ? Promise.resolve(true) : askForConfirmation()
 
-  return confirm.then((confirmed) => {
-    if (confirmed) {
-      return runActions(appActionsList, dokku, repoCache)
-    }
-  })
+  if (yes || await askForConfirmation()) {
+    await runActions(appActionsList, dokku, repoCache)
+  }
 }
 
-function askForConfirmation() {
-  return readAsync({ prompt: "apply changes (y/N)?" }).then(yn)
+async function askForConfirmation() {
+  const input = readAsync({ prompt: "apply changes (y/N)?" })
+  return yn(input)
 }
 
 function runActions(appActionsList, dokku, repoCache) {
   return bluebird.mapSeries(appActionsList, ({ appName, actions }) => {
     console.log(printName(appName))
 
-    return bluebird.mapSeries(actions, (action) =>
-      showProgress(
+    return bluebird.mapSeries(actions, async function (action) {
+      await showProgress(
         (spinner) => chalk.gray(`${printAction(action)} ${spinner}`),
         action.run(dokku, repoCache)
-      ).then(() => console.log(chalk.cyan(printAction(action))))
-    )
+      )
+
+      console.log(chalk.cyan(printAction(action)))
+    })
   })
 }
 

src/commands/envCommand.js
@@ -15,10 +15,10 @@ type EnvCommand = (
   dokku: Dokku,
   repoCache: RepoCache,
   options: any
-) => Command
+) => Promise
 
 export default function(callback: EnvCommand): Command {
-  return (dockfile, options) => {
+  return async function(dockfile, options) {
     const environment = _.chain(dockfile.environments)
       .find(["name", options["<environment>"]])
       .value()
@@ -41,15 +41,11 @@ export default function(callback: EnvCommand): Command {
       host: environment.host
     }))
 
-    return callback(descriptions, dokku, repoCache, options)
-      .then((result) => {
-        dokku.disconnect()
-        return result
-      })
-      .catch((error) => {
-        dokku.disconnect()
-        throw error
-      })
+    try {
+      return await callback(descriptions, dokku, repoCache, options)
+    } finally {
+      dokku.disconnect()
+    }
   }
 }
 

commit 5be028696e13721fa7a44c0cd087ba511b527f53

Restore working version of showProgress
The function broke in 12823317db99f16c6d0344a2ff5cdcde3450bfb9.

src/showProgress.js
@@ -1,17 +1,26 @@
 import elegantSpinner from "elegant-spinner"
 import logUpdate from "log-update"
 
-export default async function(message, promise) {
+export default function(message, promise) {
   const spinner = elegantSpinner()
 
   const interval = setInterval(() => {
     logUpdate(message(spinner()))
   }, 66)
 
-  try {
-    return await promise
-  } finally {
+  function stop() {
     clearInterval(interval)
     logUpdate.clear()
   }
+
+  return promise.then(
+    result => {
+      stop()
+      return result
+    },
+    error => {
+      stop()
+      throw error
+    }
+  )
 }