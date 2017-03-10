a1f577d3263b329de445024ecd39088c4a1b771b
Guido Lorenz <guido.lorenz@artcom.de>
2017-03-10 15:43:56 +0100

    Use early exit

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,28 +13,30 @@ import type { AppData } from "../appData"
 export type Action = ConfigAction | CreateAction | DockerOptionAction | PushAction | StartAction
 
 export function deriveActions(app: AppData): Array<Action> {
+  if (app.status === "unknown") {
+    return []
+  }
+
   const actions = []
 
-  if (app.status !== "unknown") {
-    if (app.status === "missing") {
-      actions.push(new CreateAction(app))
-    }
+  if (app.status === "missing") {
+    actions.push(new CreateAction(app))
+  }
 
-    if (!isEqual(app.description.config, app.actual.config)) {
-      actions.push(new ConfigAction(app))
-    }
+  if (!isEqual(app.description.config, app.actual.config)) {
+    actions.push(new ConfigAction(app))
+  }
 
-    if (!isEqual(app.description.dockerOptions, app.actual.dockerOptions)) {
-      actions.push(new DockerOptionAction(app))
-    }
+  if (!isEqual(app.description.dockerOptions, app.actual.dockerOptions)) {
+    actions.push(new DockerOptionAction(app))
+  }
 
-    if (app.description.version !== app.actual.version) {
-      actions.push(new PushAction(app.description))
-    }
+  if (app.description.version !== app.actual.version) {
+    actions.push(new PushAction(app.description))
+  }
 
-    if (!app.running && actions.length === 0) {
-      actions.push(new StartAction(app))
-    }
+  if (!app.running && actions.length === 0) {
+    actions.push(new StartAction(app))
   }
 
   return actions
